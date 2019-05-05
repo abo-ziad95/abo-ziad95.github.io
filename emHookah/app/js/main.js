e6711a5d881937cebfe215a682ff8c2b495f4af8
Ammar <kiravan@kai24.com>
2019-05-06 02:31:40 +0300

    responcive

--- emHookah/app/js/main.js
+++ emHookah/app/js/main.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
   $('.header-btn, .scroll-btn, .scroll').click(function(e){
     e.preventDefault()
+    $('.mob-humburger-wrap').removeClass('mob-humburger-wrap__open')
+    $('.mob-menu-wrap').removeClass('mob-menu-wrap__open')
+    $('body, html').css('overflow-x', '')
     let anchor = $(this).data('anchor')
+    $('#line').removeClass('fixed')
     $('html, body').animate({scrollTop: $(anchor).offset().top}, 500)
   });
+  $(document).on("scroll", function(e) {
+    if($(document).scrollTop() > 100) {
+      $('#line').removeClass('fixed')
+    } else{
+      $('#line').addClass('fixed')
+    }
+  });
+  $('.mob-humburger-wrap').click(() => {
+    if($('.mob-humburger-wrap').hasClass('mob-humburger-wrap__open')){
+      $('.mob-humburger-wrap').removeClass('mob-humburger-wrap__open')
+      $('.mob-menu-wrap').removeClass('mob-menu-wrap__open')
+      $('body, html').css('overflow-x', '')
+    }else{
+      $('.mob-humburger-wrap').addClass('mob-humburger-wrap__open')
+      $('.mob-menu-wrap').addClass('mob-menu-wrap__open')
+      $('body, html').css('overflow-x', 'hidden')
+    }
+  })
 })
